48cc827f375c13d62a08d9b41fe479ee2c486da6
wesselporschen <wesselporschen@gmail.com>
2026-02-23 21:37:49 +0100

    bla

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
--- options
+-- [[ options ]] --
 
 vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
@@ -12,6 +12,7 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.swapfile = false
+vim.opt.undofile = true
 vim.opt.signcolumn = "yes"
 vim.opt.ignorecase = true
 
@@ -19,17 +20,27 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.updatetime = 50
 vim.opt.scrolloff = 8
+vim.g.have_nerd_font = true
 
 vim.g.mapleader = " "
 
+-- highlight when yanking
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank({
+            timeout = 20,
+        })
+    end,
+})
 
--- general keymaps
+-- [[ general keymaps ]] --
 
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [d]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [d]iagnostic message' })
 
-
--- plugins
+-- [[ plugins ]] --
 
 vim.pack.add({
     { src = "https://github.com/stevearc/oil.nvim" },
@@ -38,11 +49,13 @@ vim.pack.add({
     { src = "https://github.com/chomosuke/typst-preview.nvim" },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/theprimeagen/harpoon" },
+    { src = "https://github.com/mbbill/undotree" },
     { src = "https://github.com/vague2k/vague.nvim" },
 })
 
 
--- style and colorscheme
+-- [[ style and colorscheme ]] --
+
 vim.cmd.colorscheme("vague")
 
 vim.api.nvim_set_hl(0, "Search", {
@@ -55,6 +68,9 @@ vim.api.nvim_set_hl(0, "IncSearch", {
     bg = "#ff8800",
 })
 
+-- undotree
+
+vim.keymap.set('n', '<leader>U', ':UndotreeToggle<CR>')
 
 -- lsp and completion
 
@@ -64,8 +80,15 @@ vim.lsp.enable({
     "clangd",
 })
 
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local opts = { buffer = args.buf }
+
+        vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover)
+    end,
+})
 
 
 -- harpoon
@@ -73,7 +96,10 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover)
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set('n', '<leader>a', mark.add_file)
+vim.keymap.set('n', '<leader>a', function()
+    mark.add_file()
+    vim.notify('File added to harpoon')
+end)
 vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
 vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
@@ -93,11 +119,10 @@ vim.keymap.set('n', '<leader>gl', ':lua vim.diagnostic.open_float()<CR>')
 
 require "mini.pick".setup()
 
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
+vim.keymap.set('n', '<leader>fh', ':Pick help<CR>')
 vim.keymap.set('n', '<leader>ff', ':Pick files<CR>')
 vim.keymap.set('n', '<leader>fg', ':Pick grep_live<CR>')
--- vim.keymap.set('n', '<leader>fG', '<cmd>Pick files tool=git<CR>')
-vim.keymap.set('n', '<leader>tt', '<cmd>Pick files tool=git<CR>')
+-- vim.keymap.set('n', '<leader>tt', '<cmd>Pick files tool=git<CR>')
 
 local pick = require('mini.pick')
 vim.keymap.set('n', '<leader>fG', function()
